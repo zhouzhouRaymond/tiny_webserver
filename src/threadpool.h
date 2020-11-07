022d16e193c7b3381a19b5b283461604ef5ebdd6
raymond_zhou <zhouyaxz@163.com>
2020-11-07 20:23:31 +0800

    添加线程池

---
+++ src/threadpool.h
@@ -0,0 +1,17 @@
+//
+// Created by Raymond on 2020/11/7.
+//
+#pragma once
+
+#ifndef TINY_WEBSERVER_SRC_THREADPOOL_H_
+#define TINY_WEBSERVER_SRC_THREADPOOL_H_
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <queue>
+#include <stdexcept>
+#include <thread>
+
+#endif  // TINY_WEBSERVER_SRC_THREADPOOL_H_

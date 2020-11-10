d921ee29b465eabc3643da9866a9562eaad27412
raymond_zhou <zhouyaxz@163.com>
2020-11-10 10:49:38 +0800

    初始化线程池类

--- src/tiny_threadpool.h
+++ src/tiny_threadpool.h
@@ -14,5 +14,23 @@
 #include <queue>
 #include <stdexcept>
 #include <thread>
+#include <vector>
+
+/* 固定数量的线程 */
+class fix_threadpool {
+ public:
+  fix_threadpool() {}
+  fix_threadpool(std::size_t _num_thread) {}
+  ~fix_threadpool() {}
+
+ private:
+  std::vector<std::thread> workers;         // 线程队列
+  std::queue<std::function<void()>> tasks;  // 任务队列
+
+  std::mutex mutex_queue;
+  std::condition_variable condition;
+  std::size_t num_thread;
+  bool stop;
+};
 
 #endif  // TINY_WEBSERVER_SRC_TINY_THREADPOOL_H_
--- test/test_threadpool.cpp
+++ test/test_threadpool.cpp
@@ -2,6 +2,9 @@
 // Created by Raymond on 2020/11/8.
 //
 #include <iostream>
+
+#include "../src/tiny_threadpool.h"
+
 int main() {
   std::cout << "test_threadpool" << std::endl;
   return 0;
